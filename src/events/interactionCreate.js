7b388d206216ca6773a50f37342a7b254cd5b646
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-09-04 13:03:29 +0300

    Handle the button interaction

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,10 +1,19 @@
 module.exports = {
   name: "interactionCreate",
   async execute(interaction) {
+    if (interaction.isButton()) {
+      if (interaction.customId === "reportButton") {
+        interaction.user.createDM().then((dmChannel) => {
+          return dmChannel.send(
+            `Administrators have seen your report in ${interaction.guild.name}`
+          );
+        });
+      }
+    }
+
     if (!interaction.isChatInputCommand()) return;
 
     const command = interaction.client.commands.get(interaction.commandName);
-
     if (!command) return;
 
     try {
