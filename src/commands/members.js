6f5b8f0586a6e752626125d3d62a5ae6c4885ca4
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-09-05 16:37:12 +0300

    Typo

--- src/commands/members.js
+++ src/commands/members.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("members")
-    .setDescription("Display info about this server."),
+    .setDescription("Indicates the number of participants of this server."),
   async execute(interaction) {
     return interaction.reply(
       `Sunucumuzda ${interaction.guild.memberCount} katılımcı var!`
