b82987330e978aa1d6cc97b5b24ef73543247c5a
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-09-09 17:44:08 +0300

    Handle the graduation of 12th grades
    Handle the case where 12th grades need to be updated, meaning graduated. If the class is 12th grade, this means there's not going to be any grater than 12 so we need to kind of graduate these 12th grades and make them from now on a guest in this server. On the other hand, return something ephemeral so that we don't have problems about non-responded interactions.

--- src/commands/update-classes.js
+++ src/commands/update-classes.js
@@ -25,17 +25,34 @@ module.exports = {
             allClassesWithName.includes(role.name)
           )
         ) {
-          const currentClass = member.roles.cache.find((role) =>isValid(role.name));
-          const nextClass = getNextClass(currentClass.name);
+          const currentClass = member.roles.cache.find((role) =>
+            isValid(role.name)
+          );
           await member.roles.remove(currentClass);
-          await member.roles.add(nextClass);
+
+          if (currentClass.name.startsWith("12")) {
+            // If the class is 12th grade, this means there's not gonna be any grade greater than 12
+            // so we need to kind of graduate these 12th grades and make them from now on a guest in this server.
+            const guestRoleId = process.env.GUEST_ROLE_ID;
+            const nextClass = interaction.guild.roles.cache.get(guestRoleId);
+            await member.roles.add(nextClass);
+          } else {
+            const nextClass = getNextClass(currentClass.name);
+            await member.roles.add(nextClass);
+          }
         }
       });
     });
 
-    interaction.guild.channels.cache.get(announcementChannelId).send(
+    await interaction.guild.channels.cache.get(announcementChannelId).send(
       "📢 Merhabalar, @everyone. Yeni dönem başlamıştır. Sınıf dereceleri yükseltildi."
       // 📢 | Hello, <@everyone>. The new term has begun. The grade of classes has been upgraded.
     );
+
+    return interaction.reply({
+      content: "Sınıf dereceleri başarılı bir şekilde yükseltildi.",
+      // The grade of classes have succesffully been upgraded.
+      ephemeral: true,
+    });
   },
 };
