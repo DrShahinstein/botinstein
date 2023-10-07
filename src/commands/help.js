35af23c3d5946b8fe47696398e2756ee34dc7471
DrShahinstein <1taylanozturk@protonmail.com>
2023-10-07 21:35:55 +0300

    Update /help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -29,33 +29,25 @@ module.exports = {
     if (interaction.options.getSubcommand() === "admin") {
       const embed = new EmbedBuilder()
         .setTitle("Komutlar")
-        .setDescription("Sadece yöneticilerin kullanabileceği komutlar.")
+        .setDescription("Sadece yöneticilerin kullanabileceği komutlar")
         .setColor(0x09dde8)
         .addFields(
           {
-            name: "/say | /söyle",
-            value: "Verilen texte göre bir mesaj gönderir.",
+            name: "/say - /söyle",
+            value: "Verilen texte göre bir mesaj gönderir",
           },
           {
-            name: "/purge | /temizle",
-            value: "99'a kadar mesaj temizleyebilir.",
+            name: "/purge - /temizle",
+            value: "99'a kadar mesaj temizleyebilir",
           },
-          { name: "/kick", value: "Üyeleri kickler." },
-          { name: "/ban", value: "Kullanıcıları banlar." },
-          { name: "/unban", value: "Kullanıcıların yasağını kaldırır." },
-          { name: "/mute", value: "Üyeleri susturur." },
-          { name: "/unmute", value: "Üyelerin susturmasını kaldırır." },
+          { name: "/kick", value: "Üyeleri kickler" },
+          { name: "/ban", value: "Kullanıcıları banlar" },
+          { name: "/unban", value: "Kullanıcıların yasağını kaldırır" },
+          { name: "/mute", value: "Üyeleri susturur" },
+          { name: "/unmute", value: "Üyelerin susturmasını kaldırır" },
           {
-            name: "/add-class | /sınıf-ekle",
-            value: "Belirlediğiniz üyeye bir sınıf rolü verir.",
-          },
-          {
-            name: "/remove-class | /sınıf-kaldır",
-            value: "Belirlediğiniz üyenin bir sınıf rolünü kaldırır.",
-          },
-          {
-            name: "/update-classes | /sınıfları-güncelle",
-            value: "Sınıfların derecesini yükseltir.",
+            name: "/update-classes - /sınıfları-güncelle",
+            value: "Sınıfların derecesini yükseltir",
           }
         );
 
@@ -63,27 +55,27 @@ module.exports = {
     } else if (interaction.options.getSubcommand() === "everyone") {
       const embed = new EmbedBuilder()
         .setTitle("Komutlar")
-        .setDescription("Herkesin kullanabileceği komutlar.")
+        .setDescription("Herkesin kullanabileceği komutlar")
         .setColor(0x09dde8)
         .addFields(
           {
             name: "/ping",
-            value: "\"Pong!\" ile cevap verir.",
+            value: '"Pong!" ile cevap verir',
           },
           {
             name: "/beep",
-            value: "\"Boop!\" ile cevap verir.",
+            value: '"Boop!" ile cevap verir',
           },
-          { name: "/members", value: "Bu sunucudaki üye sayısını gösterir." },
+          { name: "/members", value: "Bu sunucudaki üye sayısını gösterir" },
           {
-            name: "/roll-dice | zar-at",
-            value: "1-6 arasında bir sayı döndürür.",
+            name: "/roll-dice - zar-at",
+            value: "1-6 arasında bir sayı döndürür",
           },
-          { name: "/github", value: "Botinstein'ın Github linkini verir." },
-          { name: "/report", value: "Bir üyeyi yöneticilere report eder." },
+          { name: "/github", value: "Botinstein'ın Github linkini verir" },
+          { name: "/report", value: "Bir üyeyi yöneticilere report eder" },
           {
-            name: "/pick-class | /sınıf-seç",
-            value: "Size bir sınıf rolü seçer.",
+            name: "/pick-class - /sınıf-seç",
+            value: "Sınıf seviyenize ait rolü seçer",
           }
         );
       return interaction.reply({ embeds: [embed] });
